a5cc585a1c9e1ecfe4baa2d43e64214c1e8d8ffa
Jhonatabz <jhonatabezerra028@gmail.com>
2025-04-02 01:20:33 -0300

    implementado as métricas de tempo e consumo de memória

--- python/main.py
+++ python/main.py
@@ -1,15 +1,35 @@
 from sorts import bubblesort, mergesort, quicksort
 from time import time
+import tracemalloc
 import random
 
-def gerar_lista_aleatoria(tamanho=100, limite=1000):
-    return random.sample(range(1, limite), tamanho)
+
+def lista_ordenada(tamanho=1000000):
+    return list(range(1, tamanho + 1))
+
+def gerar_lista_aleatoria(tamanho=1000000, limite=1000000, repeticoes=False):
+    if repeticoes:
+        return random.choices(range(1, limite), k=tamanho)
+    return random.sample(range(0, limite), tamanho)
+
+
+def ordenar_lista(algoritmo, lista):   
+    lista_copia = lista[:]
+
+    tracemalloc.start()
+    inicio_temp = time()
+    algoritmo(lista_copia)
+    fim_temp = time()
+    mem_atual, mem_pico = tracemalloc.get_traced_memory()
+    tracemalloc.stop()
+
+    print(f'Algoritmo: {algoritmo.__name__}') 
+    print(f'Memória consumida: {mem_pico / 1024 / 1024:.4f} MiB')
+    print(f'Tempo de execução: {fim_temp-inicio_temp} Segundos\n')
 
 if __name__ == "__main__":
-    listaAleatoria = gerar_lista_aleatoria(999) # Gerando uma lista de números aleatórios
-    print("Lista original:", listaAleatoria)
+    lista_aleatoria = gerar_lista_aleatoria(10000, repeticoes=True)
 
-    inicio = time() 
-    print("Bubble Sort:", bubblesort(listaAleatoria[:])) # Alterne o algorimo de ordenação aqui
-    fim = time()
-    print(f'O tempo de execução foi: {fim-inicio}ms')
+    ordenar_lista(quicksort, lista_aleatoria)
+    ordenar_lista(mergesort, lista_aleatoria)
+    ordenar_lista(bubblesort, lista_aleatoria)
